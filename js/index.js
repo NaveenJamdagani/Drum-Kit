8defb9b4c84804bb7db85c1395f99135c032df76
Anqush Jamdagani <njamdagani@gmail.com>
2017-08-23 00:25:54 +0530

    timing function applied

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
 window.addEventListener("keydown",function(e)
 {
 	var audio = document.querySelector(`audio[data-key="${e.which}"]`);
-	console.log(audio);
 	if(!audio)return;
 	audio.currentTime = 0;
 	audio.play();
 	var key = document.querySelector(`.alphabet[data-key="${e.which}"]`);
 	key.classList.add("playing");
-	console.log(key);	
+	setTimeout(window.addEventListener("keyup",function(e)
+	{
+		var audio = document.querySelector(`audio[data-key="${e.which}"]`);
+		if(!audio)return;
+		audio.currentTime = 0;
+		audio.pause();
+		var key = document.querySelector(`.alphabet[data-key="${e.which}"]`);
+		key.classList.remove("playing");
+	}),3000);
 });
 
-window.addEventListener("keyup",function(e)
-{
-	var audio = document.querySelector(`audio[data-key="${e.which}"]`);
-	if(!audio)return;
-	audio.currentTime = 0;
-	audio.pause();
-	var key = document.querySelector(`.alphabet[data-key="${e.which}"]`);
-	key.classList.remove("playing");
-});
